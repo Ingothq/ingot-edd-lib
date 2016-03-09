6ae1a8a56838a662492ef4995a80712b3073da0e
Josh Pollock <jpollock412@gmail.com>
2016-03-08 21:18:27 -0500

    INGOT_EDD_VER constant

--- src/functions.php
+++ src/functions.php
@@ -10,6 +10,7 @@ use ingot\addon\edd\tracking;
  * @since 1.0.0
  */
 function ingot_edd_destination_init(){
+	define( 'INGOT_EDD_VER', '1.0.0' );
 	add_action( 'ingot_loaded', function(){
 		new add_destinations();
 		new tracking();
